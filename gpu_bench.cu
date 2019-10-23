a6234347bec9703d2212adf920342d5bab629cc9
huangyizhitt <422603830@qq.com>
2019-10-23 06:11:36 -0700

    [why] add gpu access [who] huangyizhi

--- gpu_bench.cu
+++ gpu_bench.cu
@@ -38,9 +38,8 @@ float test_host_to_device_uma(unsigned char *array, unsigned int bytes, int loop
 
 	checkCudaErrors(cudaEventSynchronize(start));
 	checkCudaErrors(cudaEventSynchronize(stop));
-	gpu_done = 1;
  	checkCudaErrors(cudaEventElapsedTime(&elapse, start, stop));
-	printf("bytes: %d, elapse: %f, loops: %d\n", bytes, elapse, loops);	
+	
 	bandwidth =  ((long)bytes * loops / MB) / (elapse / 1000);
 	
 	checkCudaErrors(cudaEventDestroy(stop));
@@ -54,18 +53,55 @@ void gpu_array_destroy(unsigned char *array)
 	cudaFree(array);
 }
 
+__global__ void gpu_array_read(unsigned char *array, unsigned int size)
+{
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	unsigned char value;
+	if(x < size) {
+		value = array[x];
+	}
+}
+
+float test_device_access(unsigned char *array, unsigned int size)
+{
+	int threads = 1024;
+	int blocks = (*bytes - 1) / threads + 1;
+	float elapse = 0;
+	float bandwidth = 0;
+	cudaEvent_t start, stop;
+
+	checkCudaErrors(cudaEventCreate(&start));
+	checkCudaErrors(cudaEventCreate(&stop));
+
+	checkCudaErrors(cudaEventRecord(start, 0));
+	gpu_array_read<<<blocks, threads>>>(array, bytes);
+	checkCudaErrors(cudaDeviceSynchronize());
+	checkCudaErrors(cudaEventRecord(stop, 0));
+
+	checkCudaErrors(cudaEventSynchronize(start));
+	checkCudaErrors(cudaEventSynchronize(stop));
+	gpu_done = 1;
+	bandwidth =  ((long)bytes * loops / MB) / (elapse / 1000);
+	
+	checkCudaErrors(cudaEventDestroy(stop));
+	checkCudaErrors(cudaEventDestroy(start));
+
+	return bandwidth;
+}
+
 extern "C"
 void gpu_bench(struct config *con)
 {
-	unsigned int bytes = con->size * sizeof(long);
+	unsigned int bytes = con->size * sizeof(long); 
 	float bandwidth;
 	unsigned char *array = gpu_array_make_uma(con->size * sizeof(long));
 	bandwidth = test_host_to_device_uma(array, bytes, con->loops);
 	printf("Host to device: %.3f MiB/s\n", bandwidth);
+
+	bandwidth = test_device_access(array, bytes, con->loops);
+	printf("device access memory: %.3f MiB/s\n", bandwidth);
+	
 	gpu_array_destroy(array);
 }
 
-void test()
-{
 
-}
